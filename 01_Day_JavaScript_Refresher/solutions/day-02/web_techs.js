f0a54b0b5c1935e6af72a45e20d58e46e9d14774
Damian Ponce <dponcez_git@hotmail.com>
2022-05-02 19:03:25 -0500

    Chore: Exercise level2 complete

---
+++ 01_Day_JavaScript_Refresher/solutions/day-02/web_techs.js
@@ -0,0 +1,14 @@
+// create a print function and get the output values on JavaScript console
+const print = value => console.log(value);
+// create a web techs array
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+];
+
+print(webTechs)
