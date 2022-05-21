e08689f7ddebd869e9777aa0b072f920df5a8d99
Damian Ponce <dponcez_git@hotmail.com>
2022-05-21 01:11:27 -0500

    chore: exercise [level1 and level2] complete

--- 07_Day_Class_Components/solutions/day-07/src/App.js
+++ 07_Day_Class_Components/solutions/day-07/src/App.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import UserCard from './components/UserCard'
+import userImage from './images/asabeneh.jpg'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+class App extends React.Component {
+    showDate = (time) => {
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ]
+
+        const month = months[time.getMonth()].slice(0, 3);
+        const year = time.getFullYear();
+        const date = time.getDate();
+
+        return `${month} ${date}, ${year}`
+    }
+    render(){
+        const webTechs = [
+            'HTML', 'CSS', 'Sass', 'JS', 
+            'React', 'Redux', 'Node',
+            'MongoDB', 'Python', 'Flask',
+            'Django', 'NumPy', 'Pandas',
+            'Data Analysis', 'MYSQL', 'GraphicQL',
+            'D3.js', 'Gatsby', 'Docker', 'Heroku', 'Git'
+        ]
+
+        const userInfo = {
+            name: 'asabeneh yetayeh',
+            info: 'senior developer',
+            image: userImage,
+            country: 'finland'
+        }
+
+        const technologies = webTechs.map((techs, index) => (
+            <button className='btn' key={index}>{techs}</button>
+        ))
+        
+        return(
+            <div className='App'>
+                <UserCard
+                    userInfo={userInfo}
+                    webTechs={technologies}
+                    showDate={this.showDate(new Date())}
+                />
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ 07_Day_Class_Components/solutions/day-07/src/components/UserCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styles/UserCard.css'
+
+const Buttons = ({webTechs}) => <>{webTechs}</>
+
+const Footer = ({showDate}) => (
+    <div className='date--container'>
+        <span className='fa-solid fa-clock'></span>
+        <p>Joined on <time>{showDate}</time></p>
+    </div>
+)
+
+const UserImage = ({userInfo}) => (
+    <figure>
+        <img className="user--image" src={userInfo.image} alt={userInfo.name}/>
+        <figcaption>
+            <h4 className="card--name">{userInfo.name}</h4>
+            <input type="checkbox"/>
+            <p className="card--info">{userInfo.info}, {userInfo.country}</p>
+        </figcaption>
+    </figure>
+)
+
+class UserCard extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div className="card--bg">
+                <article className="card--container">
+                    <div className="card--image__container">
+                        <UserImage userInfo={this.props.userInfo}/>
+                    </div>
+                    <h4>skills</h4>
+                    <div className='buttons--container'>
+                        <Buttons webTechs={this.props.webTechs}/>
+                    </div>
+                    <Footer showDate={this.props.showDate}/>
+                </article>
+                </div>
+        );
+    }
+}
+
+export default UserCard;
