d3eed35ce3be203bb3665d1f4c0ed87c93786e2a
Damian Ponce <dponcez_git@hotmail.com>
2022-06-15 22:06:37 -0500

    chore: create folder structure and add files.

---
+++ 11_Day_Events/solutions/day-11/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MouseEvent from './components/MouseEvent/MouseEvent'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <MouseEvent/>
+    </div>
+  )
+}
+
+export default App
