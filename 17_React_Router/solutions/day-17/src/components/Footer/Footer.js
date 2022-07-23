1076959b8ee59a7d971ac19f6a724ee38f0e3c21
Damian Ponce <dponcez_git@hotmail.com>
2022-07-23 02:43:10 -0500

    chore: add [.scss] & delete [babel.config.js] file

--- 17_React_Router/solutions/day-17/src/components/Footer/Footer.js
+++ 17_React_Router/solutions/day-17/src/components/Footer/Footer.js
@@ -1,27 +1,31 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import '../../styles/Footer.scss'
 
-// const socialBrands = {
-//     facebook: facebookIcon,
-//     twitter: twitterIcon,
-//     instagram: instagramIcon,
-//     github: githubIcon
-//   }
-
-const Footer = () => {
-  // const getBrandObject = Object.keys(socialBrands)
-  // const getSocialBrands = getBrandObject.map((icons, index) => (
-  //   <a key={index} href='#'>{icons}</a>
-  // ))
+const Footer = () => {  
   return (
     <div className="Footer">
       <div className="Footer--container">
         <div className="social--media">
-          <a href={facebookIcon}></a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faFacebook}/>
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faTwitter}/>
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faGithub}/>
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faWhatsapp}/>
+          </a>
         </div>
         <div className='copyright'>
-          <span>&copy</span>
-          <p>damian ponce</p>
+          <p>
+            <span>&copy;</span> damian ponce - 
+            <time dateTime='2022-07-23'>2022</time>
+          </p>
         </div>
       </div>
     </div>
