04497686951c2b11a5a3cd8993164ae5708fd8b4
Damian Ponce <dponcez_git@hotmail.com>
2022-08-15 17:48:05 -0500

    chore: add dependencies

--- 19_projects/solutions/day-19/src/App.js
+++ 19_projects/solutions/day-19/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 const App = () => {
   return (
-    <div className='App'>App</div>
+    <div className='App'>
+      <Main/>
+      <Footer/>
+    </div>
   )
 }
 
